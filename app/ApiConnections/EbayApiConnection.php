3f0e8a6e7a80f9d22e9dbd2ed45304402891ab15
Johnny Louweret <johnny@syncbnb.com>
2022-02-08 07:26:34 +0200

    pull items from Ebay in the EbayApiConnection

--- app/ApiConnections/EbayApiConnection.php
+++ app/ApiConnections/EbayApiConnection.php
@@ -2,22 +2,52 @@
 
 namespace App\ApiConnections;
 
-use App\ApiConnections\Interfaces\ApiProductInterface;
+use App\ApiConnections\Interfaces\ApiProduct;
+use App\Collection\ProductCollection;
 use App\Objects\ApiHeaders\EbayHeaders;
+use App\Objects\Operation;
 use App\Objects\ApiRequest\ProductApiRequest;
-use Illuminate\Http\Client\Response;
+use App\Objects\Enum\EbayStatusEnum;
+use App\Objects\Enum\HttpCodeEnum;
+use App\Objects\Parser\EbayParser;
+use App\Utility\XmlUtility;
+use Exception;
 
-class EbayApiConnection extends ApiConnection implements ApiProductInterface
+class EbayApiConnection extends ApiConnection implements ApiProduct
 {
+    /**
+     * Errors.
+     */
+    const ERROR_SOMETHING_WENT_WRONG = 'something went wrong.';
+
     /**
      * @param ProductApiRequest $productApiRequest
      *
-     * @return Response
+     * @return Operation
+     * @throws Exception
      */
-    public function getProduct(ProductApiRequest $productApiRequest): Response
+    public function getProduct(ProductApiRequest $productApiRequest): Operation
     {
+        $operation = new Operation();
+
         $productApiRequest->setHeader(EbayHeaders::createWithAuthentication(env('EBAY_KEY')));
 
-        return self::get($productApiRequest);
+        $productResponse = self::get($productApiRequest);
+
+        if (HttpCodeEnum::HTTP_OK === $productResponse->status()) {
+            $responseArray = XmlUtility::xmlToArray($productResponse);
+
+            if (EbayStatusEnum::STATUS_SUCCESS === EbayParser::getStatusFromResult($responseArray)) {
+                $items = EbayParser::getProductsFromResult($responseArray);
+                $operation->setProducts(ProductCollection::createFromArray($items));
+
+            } else {
+                $operation->setError(EbayParser::getErrorMessageFromResult($responseArray));
+            }
+        } else {
+            $operation->setError(self::ERROR_SOMETHING_WENT_WRONG);
+        }
+
+        return $operation;
     }
 }
